36564b060466b9521453fb82f26f4bb7e482b11e
Gustavo Viana <gustavofvv@gmail.com>
2022-08-22 23:08:08 +0000

    feat: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentsModule } from './students/students.module';
-import { TransactionsModule } from './transactions/transactions.module';
+import { StudentsModule } from './modules/students/students.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
   imports: [StudentsModule, TransactionsModule],
